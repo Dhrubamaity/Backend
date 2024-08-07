43d4fdffff3839ec088cfdba805a5fa37c6be32d
Dhrubajyoti Maity <maitydhruba33@gmail.com>
2024-08-07 13:00:58 +0530

    build up show post with uuid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const{v4:uuidv4}= require('uuid');
 const path = require("path");
 
 const port = 8080;
@@ -11,28 +12,40 @@ app.use(express.urlencoded({extended:true}));
 
 let posts = [
     {
+        id:         uuidv4(),
         username:   "Dhruba",
         content:    "Iam a full stack developer"        
     },
     {
+        id:         uuidv4(),
         username:   "Nayan",
         content:    "Iam a MALopps developer" 
     },
     {
+        id:         uuidv4(),
         username:   "Swarna",
         content:    "Iam a java developer" 
     },
 ]
 
-app.post("/posts",(req,res)=>{
-    let {username,content}= req.body;
-    posts.push({username,content});
-    res.redirect("/posts");
-});
 
 app.get("/posts/new",(req,res)=>{
     res.render("new.ejs");
 });
+
+app.post("/posts",(req,res)=>{
+    let id = uuidv4();
+    let {username,content}= req.body;
+    posts.push({id,username,content});
+    res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    console.log(id);
+    let post = posts.find((p)=> id === p.id);
+    res.render("show.ejs",{post});
+});
+
 
 
 app.get("/posts",(req,res)=>{
