dc751337af6d33f503549ea5f71e6a6bb7e042a9
Dhrubajyoti Maity <maitydhruba33@gmail.com>
2024-08-10 21:29:11 +0530

    added edit and delete functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const{v4:uuidv4}= require('uuid');
 const path = require("path");
+const methodOverride = require("method-override");
 
 const port = 8080;
 
@@ -9,6 +10,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 
 let posts = [
     {
@@ -28,6 +30,24 @@ let posts = [
     },
 ]
 
+app.delete("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    posts = posts.filter((p)=> id !== p.id);
+    res.redirect("/posts");
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    let post = posts.find((p)=> id === p.id);
+    let newcontent = req.body.content;
+    post.content = newcontent;
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}= req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs",{post});
+});
 
 app.get("/posts/new",(req,res)=>{
     res.render("new.ejs");
@@ -45,8 +65,6 @@ app.get("/posts/:id",(req,res)=>{
     let post = posts.find((p)=> id === p.id);
     res.render("show.ejs",{post});
 });
-
-
 
 app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts});
